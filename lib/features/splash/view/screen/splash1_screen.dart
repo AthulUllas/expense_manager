06f00356b69a889d7221263222089219f4205f77
AthulUllas <athulullas72@gmail.com>
2026-02-26 18:47:52 +0530

    Partially completed the splash1 screen UI

--- lib/features/splash/view/screen/splash1_screen.dart
+++ lib/features/splash/view/screen/splash1_screen.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Splash1Screen extends StatelessWidget {
+class Splash1Screen extends HookWidget {
   const Splash1Screen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final contents = [
+      "Privacy by Default, With Zero Ads or Hidden Tracking",
+      "Insights That Help You Spend Better Without Complexity",
+      "Local First Tracking That Stays Fully on Your Device",
+    ];
+
+    final descriptions = [
+      "No ads. No tracker. No third-party-analytics",
+      "See category-wise spending, recent activity..",
+      "Your finances stay on your phone.",
+    ];
+
+    final currentIndex = useState<int>(0);
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 18, 18, 18),
       body: Container(
@@ -16,6 +30,7 @@ class Splash1Screen extends StatelessWidget {
           ),
         ),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
@@ -33,11 +48,87 @@ class Splash1Screen extends StatelessWidget {
                       child: Center(
                         child: Text(
                           "SKIP",
-                          style: TextStyle(color: Colors.white, fontSize: 24),
+                          style: GoogleFonts.anekLatin(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
                         ),
                       ),
                     ),
                   ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 2.5,
+                        margin: EdgeInsets.only(left: 20, right: 4),
+                        decoration: BoxDecoration(
+                          color: currentIndex.value == 0
+                              ? Colors.white.withOpacity(1.0)
+                              : Colors.white.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 2.5,
+                        margin: EdgeInsets.only(left: 4, right: 4),
+                        decoration: BoxDecoration(
+                          color: currentIndex.value == 1
+                              ? Colors.white.withOpacity(1.0)
+                              : Colors.white.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 2.5,
+                        margin: EdgeInsets.only(left: 4, right: 20),
+                        decoration: BoxDecoration(
+                          color: currentIndex.value == 2
+                              ? Colors.white.withOpacity(1.0)
+                              : Colors.white.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 24),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20, right: 16),
+                        child: Text(
+                          contents[currentIndex.value],
+                          style: GoogleFonts.plusJakartaSans(
+                            color: Colors.white,
+                            fontSize: 36,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          softWrap: true,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 20),
+                        child: Text(descriptions[currentIndex.value]),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
